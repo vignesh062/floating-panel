e3053a81fd0cf3b214df1e59b373649b08d56137
Hitesh Balar <hsbalar@gmail.com>
2018-11-08 17:50:12 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,55 +10,36 @@ declare var $:any;
 })
 export class AppComponent implements OnInit {
   title = "app";
+  
 
   ngOnInit() {
 
         $(document).ready(function() {
-          $('#mydiv').draggable({handle: window});
+          $('#mydiv').draggable({
+            opacity: 0.7,
+            containment : "document",
+          });
         })
-        // dragElement(document.getElementById("mydiv"));
-      // function dragElement(elmnt) {
-      //   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-      //   if (document.getElementById(elmnt.id + "header")) {
-      //     // if present, the header is where you move the DIV from:
-      //     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-      //   } else {
-      //     // otherwise, move the DIV from anywhere inside the DIV:
-      //     elmnt.onmousedown = dragMouseDown;
-      //   }
-      //   function dragMouseDown(e) {
-      //     e = e || window.event;
-      //     e.preventDefault();
-      //     // get the mouse cursor position at startup:
-      //     pos3 = e.clientX;
-      //     pos4 = e.clientY;
-      //     document.onmouseup = closeDragElement;
-      //     // call a function whenever the cursor moves:
-      //     document.onmousemove = elementDrag;
-      //   }
-      //   function elementDrag(e) {
-      //     e = e || window.event;
-      //     e.preventDefault();
-      //     // calculate the new cursor position:
-      //     pos1 = pos3 - e.clientX;
-      //     pos2 = pos4 - e.clientY;
-      //     pos3 = e.clientX;
-      //     pos4 = e.clientY;
-      //     // set the element's new position:
-      //     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-      //     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-      //   }
-      //   function closeDragElement() {
-      //     // stop moving when mouse button is released:
-      //     document.onmouseup = null;
-      //     document.onmousemove = null;
-      //   }
-      // }
-    }
-    
-    // minimize(e){
-    //   console.log(e);
-    //   $('#content').draggable();
-    // }
+        $('#size').click(function() {
+          $('#content').toggle('up');
+      });
 
+      $('#minimize').click(function() {
+        let windowSize = $(window).width();
+        let finalSize = windowSize - 450;
+        $('#content').hide();
+        $('#mydiv').animate({
+          position: "absolute",
+          top: "10px",
+          left: `${finalSize}px`
+        })
+        // $('#mydiv').animate({
+        //   position: "absolute",
+        //   top: "0px",
+        //   left: "0px"
+        //   // left: "1395px"
+        // })
+    });
+      }
+      
 }
